ebf40d56112fbb828f4f2f18f4677e615686974f
unknown <tomukesh5@gmail.com>
2018-01-23 14:39:47 -0500

    Saving work.

---
+++ app/src/main/java/com/cardyapp/Activities/OTPActivity.java
@@ -0,0 +1,12 @@
+package com.cardyapp.Activities;
+
+/**
+ * Created by Priyanka on 1/23/2018.
+ */
+
+public class OTPActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResourceId() {
+        return 0;
+    }
+}
